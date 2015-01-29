bf4bf7bf46bffc636f9a6678f200c38597fcd9f7
amanb014@gmail.com <amanb014>
2015-01-29 18:55:44 -0500

    Testing Push - Aman

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         for (int i = 0; i < 10; i++)
-            System.out.println(i);
+            System.out.println(i % 2);
     }
 	// write your code here
 }
