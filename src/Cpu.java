bd24b925b3c9a46e6985b996245d7c0aa9b3b96b
Michael Gwyn <Mikegwyn17@gmail.com>
2015-02-03 12:30:59 -0500

    pushed to cpu branch. started working on fetch and decode methods as well as number conversion

--- src/Cpu.java
+++ src/Cpu.java
@@ -3,5 +3,47 @@
  */
 public class Cpu
 {
-    
+    public String memory[] = {"0xC050005C", "0x4B060000"};
+    public int sReg;
+    public int dReg;
+    public int oppCode;
+    public String instructionType;
+
+    public String fetch (int pc)
+    {
+        // temporary string used for manipulating the instruction
+        String tempInstr = "";
+        tempInstr = memory[pc];
+
+        // string used as output of function
+        String fetchedInstr = tempInstr;
+
+        // point program counter to next instruction
+        pc++;
+
+        // return fetched instruction
+        return fetchedInstr;
+    }
+
+    public String decode (String fetchedInstr)
+    {
+        // temporary sting used for manipulating the instruction
+        String tempInstr;
+
+        // string used for output of function
+        String decodedInstr = "";
+
+        fetchedInstr = decodedInstr;
+
+        // return decoded instruction
+        return decodedInstr;
+    }
+
+    public void execute ()
+    {
+
+    }
+
 }
+
+
