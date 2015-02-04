a8d4ba5f8fa971afed623cca97270e9b46f09ef3
Michael Gwyn <Mikegwyn17@gmail.com>
2015-02-03 23:26:15 -0500

    reverting to previous commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,23 +2,10 @@ package com.company;
 
 public class Main {
 
-    //THE MAIN CLASS IS THE DRIVER OF THE COMPUTER.
-
-    public static void main(String[] args)
-    {
-
-//        String main = "0xC050005C";
-//        String newMain = main.substring(2);
-//        String some = "123";
-//        long correct = 0;
-//        String foo = "";
-//        correct = Long.parseLong(newMain,16);
-//        foo = Long.toBinaryString(correct);
-//
-//        System.out.println(newMain);
-//
-//        System.out.println(correct);
-//        System.out.println(foo);
-
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        for (int i = 0; i < 10; i++)
+            System.out.println(i % 2);
     }
+	// write your code here
 }
