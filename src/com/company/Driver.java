4e5b784d66d83d9ab3f9b678c02ed6f17ef3304b
Michael Gwyn <Mikegwyn17@gmail.com>
2015-02-03 19:36:46 -0500

    still attempting to reset master

---
+++ src/com/company/Driver.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by Aman on 2/3/2015.
+ */
+public class Driver {
+
+    public Driver() {
+        Cpu cpu = new Cpu();
+        Loader loader = new Loader();
+
+        runLoop();
+    }
+
+    public void runLoop() {
+
+
+    }
+}
---
+++ src/com/company/Loader.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.io.File;
+
+/**
+ * Created by Aman on 2/3/2015.
+ */
+public class Loader {
+
+    public Loader(File file) {
+
+    }
+
+}
