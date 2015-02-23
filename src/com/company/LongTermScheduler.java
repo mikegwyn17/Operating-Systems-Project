bd058e9e8e03c5144dbee388e83bbe989a658b2d
NotFixable <evanross@wireless-027-010.spsu.edu>
2015-02-23 17:06:27 -0500

    Added Ram and Long-Term Scheduler.

---
+++ src/com/company/LongTermScheduler.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by evanross on 2/23/15.
+ */
+public class LongTermScheduler {
+
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
