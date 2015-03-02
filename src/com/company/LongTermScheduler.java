c265fe5d523abf3e14aca1fe8e02f30e34df42bf
NotFixable <evanross@Skarmory.local>
2015-03-01 21:52:23 -0500

    Updated RAM and created outline for the long-term scheduler.

--- src/com/company/LongTermScheduler.java
+++ src/com/company/LongTermScheduler.java
@@ -4,6 +4,27 @@ package com.company;
  * Created by evanross on 2/23/15.
  */
 public class LongTermScheduler {
+    //Average rate of process creation
+    //Average rate of process departure rate
+    // I/O Bound Processes
+    // CPU Bound Processes
 
 
+    public void PullProcess(){
+        //Pulls process from the HDD
+    }
+
+
+    public void MultiProgramming() {
+//Degree of multiprogramming
+// number of processes in memory
+//  Process creation must be equal to the average departure rate
+// Compare last departed process with processes in the HDD to decide next process to create
+}
+
+
+
+    public void SendToRAM(){
+        //sends departing process to the RAM
+    }
 }
--- src/com/company/Ram.java
+++ src/com/company/Ram.java
@@ -4,28 +4,22 @@ package com.company;
  * Created by evanross on 2/23/15.
  */
 public class Ram {
+
     private String data;
-    private String[] ram = new String[1024];
+    private String[] RAM;
 
     public Ram(){
-        ram = null;
-        //data will be null when 1st create ram
+        data = null;
+        RAM = new String[1024];
     }
-    public  String returnData(int index){
-        return ram[index];
-        //return a disk data in that sector in the array
+    public String returnData(int index){
+        return RAM[index];
     }
-    public String readRam(){
-        //String s = this.returnData();
-        System.out.print("test");
-        return "s";
-        //read a data from ram and copy it into CPU
+    public String readRam(int index){
+        return RAM[index];
     }
     public void writeRam(String s, int index){
-        ram[index] = s;
-        //read string from a text file and write it into CPU
+        RAM[index] = s;
     }
-    public void copyRam(String s){
-        //may or not be needed
-    }
+
 }
