c138b426710824133b3454c092cc21a9a455c37d
Michael Gwyn <Mikegwyn17@gmail.com>
2015-03-08 12:54:48 -0400

    correctly added all files to master branch

--- src/com/company/Driver.java
+++ src/com/company/Driver.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import java.util.List;
+
 public class Driver {
+    public static Disk disk = new Disk();
+    public static PCB pcb = new PCB();
 
-    public static void main(String[] args) {
+    public static void main(String[] a) {
+        String program = "program.txt";
+        Loader loader = new Loader(program);
 
-        String file = "program.txt";
-        Loader loader = new Loader(file);
+        int pcbSize = pcb.getNumberOfJobs();
+
+        for(int i = 1; i <= pcbSize; i++) {
+            System.out.println(pcb.getPCB(i).toString());
+        }
     }
 }
